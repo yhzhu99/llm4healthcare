ec42d5b1094c1fcba05729946215e2db92b394cf
Yinghao Zhu <yhzhu99@gmail.com>
2023-12-23 20:32:58 +0800

    feat: files needed for prompt

---
+++ prompts/prompt.py
@@ -0,0 +1,17 @@
+SYSTEMPROMPT = "You are an experienced doctor in the field of COVID-19 treatment."
+USERPROMPT = {
+    '1-1': 'prompts/1-1_prompt.txt',
+    'N-1_batches': 'prompts/N-1_batches_prompt.txt',
+    'N-1_list': 'prompts/N-1_list_prompt.txt',
+    'N-1_string': 'prompts/N-1_string_prompt.txt',
+}
+EXAMPLE = {
+    '1-1': 'prompts/1-1_example.txt',
+    'N-1_batches': 'prompts/N-1_batches_example.txt',
+    'N-1_list': 'prompts/N-1_list_example.txt',
+    'N-1_string': 'prompts/N-1_string_example.txt',
+}
+
+UNIT_RANGE_PROMPT = "Each time I provide you with the following information:\n"
+UNIT = 'prompts/unit.json'
+RANGE = 'prompts/range.json'
