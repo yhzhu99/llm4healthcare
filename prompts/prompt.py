9db7bef4cc676fbb3db3aef8812889e4a68d269e
Yinghao Zhu <yhzhu99@gmail.com>
2023-12-27 15:36:10 +0800

    fix: handle examples and info of both datasets

--- prompts/prompt.py
+++ prompts/prompt.py
@@ -6,12 +6,26 @@ USERPROMPT = {
     'N-1_string': 'prompts/N-1_string_prompt.txt',
 }
 EXAMPLE = {
-    '1-1': 'prompts/1-1_example.txt',
-    'N-1_batches': 'prompts/N-1_batches_example.txt',
-    'N-1_list': 'prompts/N-1_list_example.txt',
-    'N-1_string': 'prompts/N-1_string_example.txt',
+    'tjh': {
+        '1-1': 'prompts/tjh/1-1_example.txt',
+        'N-1_batches': 'prompts/tjh/N-1_batches_example.txt',
+        'N-1_list': 'prompts/tjh/N-1_list_example.txt',
+        'N-1_string': 'prompts/tjh/N-1_string_example.txt',
+    },
+    'mimic-iv': {
+        '1-1': 'prompts/mimic-iv/1-1_example.txt',
+        'N-1_batches': 'prompts/mimic-iv/N-1_batches_example.txt',
+        'N-1_list': 'prompts/mimic-iv/N-1_list_example.txt',
+        'N-1_string': 'prompts/mimic-iv/N-1_string_example.txt',
+    },
 }
 
 UNIT_RANGE_PROMPT = "Each time I provide you with the following information:\n"
-UNIT = 'prompts/unit.json'
-RANGE = 'prompts/range.json'
+UNIT = {
+    'tjh': 'prompts/tjh/unit.json',
+    'mimic-iv': 'prompts/mimic-iv/unit.json',
+}
+RANGE = {
+    'tjh': 'prompts/tjh/range.json',
+    'mimic-iv': 'prompts/mimic-iv/range.json',
+}
