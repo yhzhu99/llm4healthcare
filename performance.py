30c35eb1190f6b60dcd11d576ea559e21156ea9d
Yinghao Zhu <yhzhu99@gmail.com>
2024-01-02 00:43:16 +0800

    fix: update perf saving root

--- performance.py
+++ performance.py
@@ -13,17 +13,15 @@ def export_performance(
     config = logits['config']
     labels = logits['labels']
     preds = logits['preds']
-    performance = pd.DataFrame()
-    for i, (label, pred) in enumerate(zip(labels, preds)):
-        performance = pd.concat([performance, pd.DataFrame(data=get_all_metrics(
-            preds=pred,
-            labels=label,
-            task='outcome',
-            los_info=None
-        ), index=[f'Visit {i + 1}'])], axis=0)
-    dst_path = os.path.join(dst_root, config['model'], config['prediction'] + '_' + config['format'], 'oneshot' if config['shot'] else 'zeroshot')
+    performance = pd.DataFrame(data=get_all_metrics(
+        preds=preds,
+        labels=labels,
+        task='outcome',
+        los_info=None
+    ), index=[0])
+    dst_path = os.path.join(dst_root, config['dataset'], config['task'], config['model'])
     os.makedirs(dst_path, exist_ok=True)
     performance.to_csv(os.path.join(dst_path, f'{Path(dst_path).name}.csv'))
 
 if __name__ == '__main__':
-    export_performance('logits/gpt-3.5-turbo-16k/N-1_list/oneshot/20231218-150859.pkl')
+    export_performance('/data/wangzx/llm4healthcare/logits/tjh/outcome/gpt-4-1106-preview/20240102-001910.pkl')
