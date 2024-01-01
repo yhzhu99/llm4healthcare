3af05fe507b98ddb75417adf1730dfac8a780167
Yinghao Zhu <yhzhu99@gmail.com>
2024-01-01 23:47:55 +0800

    fix: update example index

--- prompts/prompt.py
+++ prompts/prompt.py
@@ -21,23 +21,16 @@ TASK_DESCRIPTION_AND_RESPONSE_FORMAT = {
     'los': 'Your task is to Evaluate the provided medical data to estimate the remaining duration of the ICU stay. Consider the progression of health across multiple visits to forecast the length of intensive care needed. Please respond with an integer indicating the number of days expected in the ICU.',
     'readmission': 'Your task is to analyze the medical history to predict the probability of readmission within 30 days post-discharge. Include cases where a patient passes away within 30 days from the discharge date. Please respond with a floating-point number between 0 and 1, where a higher number suggests a greater likelihood of readmission.',
 }
-# USERPROMPT = {
-#     '1-1': 'prompts/1-1_prompt.txt',
-#     'N-1_batches': 'prompts/N-1_batches_prompt.txt',
-#     'N-1_list': 'prompts/N-1_list_prompt.txt',
-#     'N-1_string': 'prompts/N-1_string_prompt.txt',
-# }
-# EXAMPLE = {
-#     'tjh': {
-#         '1-1': 'prompts/tjh/1-1_example.txt',
-#         'N-1_batches': 'prompts/tjh/N-1_batches_example.txt',
-#         'N-1_list': 'prompts/tjh/N-1_list_example.txt',
-#         'N-1_string': 'prompts/tjh/N-1_string_example.txt',
-#     },
-#     'mimic-iv': {
-#         '1-1': 'prompts/mimic-iv/1-1_example.txt',
-#         'N-1_batches': 'prompts/mimic-iv/N-1_batches_example.txt',
-#         'N-1_list': 'prompts/mimic-iv/N-1_list_example.txt',
-#         'N-1_string': 'prompts/mimic-iv/N-1_string_example.txt',
-#     },
-# }
+
+EXAMPLE = {
+    'tjh': {
+        'batches': 'prompts/tjh/N-1_batches_example.txt',
+        'list': 'prompts/tjh/N-1_list_example.txt',
+        'string': 'prompts/tjh/N-1_string_example.txt',
+    },
+    'mimic-iv': {
+        'batches': 'prompts/mimic-iv/N-1_batches_example.txt',
+        'list': 'prompts/mimic-iv/N-1_list_example.txt',
+        'string': 'prompts/mimic-iv/N-1_string_example.txt',
+    },
+}
